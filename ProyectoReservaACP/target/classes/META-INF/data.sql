faff64050fd47aa0dc1a335934ccf2d932b8c577
Miguel Casas <mcasaspinillos@gmail.com>
2019-09-26 04:45:24 -0500

    version 12

--- ProyectoReservaACP/target/classes/META-INF/data.sql
+++ ProyectoReservaACP/target/classes/META-INF/data.sql
@@ -19,3 +19,8 @@ insert into usuario (idusuario,email,flagadmin,iddocumento,nameapmaterno,nameapp
 insert into usuario (idusuario,email,flagadmin,iddocumento,nameapmaterno,nameappaterno,nameusuario,password,phone) values(default,'JosePS@gmail.com',true,'201910723','Perez','Smith','Jose','78954321','955888776');
 
 insert into producto (idProducto,nameproducto,dayReabastecimiento,quantityStock,moneyPrecio,quantityReserva) values(default,'Palta','2019/10/10','100',150.0,'90');
+
+insert into reserva (idReserva,dayReserva,idUsuario,idCiudad,dayLlegada,flagAnulado,dayVencimiento) values(default,'2019/10/10',1,2,'2019/11/10',false,'2019/10/13');
+
+insert into factura (idFactura,idReserva,moneyPrecioFinal,flagPagoTarjeta,numFactura,numSerie,flagCancelado,dayFactura) values(default,1,1500.0,true,'F0001','S0001',true,'2019/09/27');
+
